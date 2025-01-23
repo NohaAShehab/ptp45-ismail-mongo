c809f93a04f538cd23c9e8e7d77888cfd5efd960
Noha Shehab <nohashehab.iti@gmail.com>
2025-01-23 12:15:14 +0200

    intro to mongo

---
+++ demoday01.js
@@ -0,0 +1,98 @@
+
+// show available databases ??
+/*
+show dbs;
+
+admin   40.00 KiB
+config  72.00 KiB
+local   40.00 KiB
+
+mongo provide 3 default databases ??
+---> 
+
+mongodb --> create database only if it contains data
+
+
+*/
+
+/**
+ * 
+ * to create database 
+ * use iti;
+ * 
+ */
+
+
+
+
+/****
+ * 
+ * 
+ * create new collection --> have documents 
+ * 
+ * use iti ; 
+ * # create collection and insert one document 
+ * db.students.insertOne({name:"noha", "track": "ui"})
+ * 
+ * this will create collection --> students 
+ * --> document --> with given values 
+ * 
+ */
+
+
+/**
+ * 
+ *  --> display all documents in the collection 
+ * select * from table ; 
+ * 
+ * db.students.find();
+ * 
+ */
+
+
+
+
+/**
+ * 
+ * show collections in the database ?
+ * show collections
+ * 
+ *  ---> create collection 
+ * db.createCollection("collname");
+ * 
+ */
+
+
+/**
+ * 
+ *  insert many objects 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
